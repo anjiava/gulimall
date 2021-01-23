4e3abf0269003e2bce6e066cbd9704474e49ba60
sherlock <865088895@qq.com>
2021-01-23 15:35:08 +0800

    项目初始化

---
+++ gulimall-coupon/src/test/java/org/mili/member/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mili.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
